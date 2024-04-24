aa08113b1a09fd36a83974ebe78e18056fde788a
felipe6san <felipe6san@hotmail.com>
2024-04-23 21:32:42 -0300

    BancoDeDadosExercicio4

--- Banco de Dados/Exercicios03.sql
+++ Banco de Dados/Exercicios03.sql
@@ -1,127 +1,83 @@
 /****Exercicio 1****/
-
 SELECT * from clientes
 
 /****Exercicio 2****/
-
 select * from FILME
 
 /****Exercicio 3****/
-
 select * from CATEGORIA
 
 /****Exercicio 4****/
-
 select * from CLIENTES where sexo = 'M'and cidade = 'Sorocaba'
 
 /****Exercicio 5****/
-
 select NOME, TELEFONE 
-
-FROM CLIENTES
-
-ORDER BY NOME ASC
+from CLIENTES
+order by NOME asc
 
 /****Exercicio 6****/
-
 select * from clientes where sexo = 'F'
 
 /****Exercicio 7****/
-
 select * from CATEGORIA 
-
 order by NOME_CATEGORIA asc
 
 /****Exercicio 8****/ 
-
 select * from FILME
-
 where DIRETOR = 'Shawn Levy' and RESERVADA = 'n'
 
 /****Exercicio 9****/ 
-
 select * from FILME
-
 where VALOR_LOCACAO > 5 and RESERVADA = 'n'
 
 /****Exercicio 10****/ 
-
 select distinct DIRETOR from filme
 
 /****Exercicio 11****/ 
-
 select count(reservada) from FILME
-
 where RESERVADA = 'n'
 
 /****Exercicio 12****/ 
-
 select max(valor_locacao) from filme
 
 /****Exercicio 13****/ 
-
 select FILME, NOME_CATEGORIA from FILME,CATEGORIA
-
 where NOME_CATEGORIA = 'Ação'
 
 /****Exercicio 14****/ 
-
-select FILME, NOME_CATEGORIA, RESERVADA, VALOR_LOCACAO FROM FILME,CATEGORIA
-
-WHERE RESERVADA = 's' and VALOR_LOCACAO < 5 and (NOME_CATEGORIA = 'Ação' or NOME_CATEGORIA ='Romance')
+select FILME, NOME_CATEGORIA, RESERVADA, VALOR_LOCACAO from FILME,CATEGORIA
+where RESERVADA = 's' and VALOR_LOCACAO < 5 and (NOME_CATEGORIA = 'Ação' or NOME_CATEGORIA ='Romance')
 
 /****Exercicio 15****/ 
-
 select FILME, NOME from CLIENTES,LOCACOES,FILME
-
 where LOCACOES.COD_CLIENTE = CLIENTES.COD_CLIENTE AND NOME = 'Renata Cristina' AND  FILME.COD_FILME = LOCACOES.COD_FILME
  
 /****Exercicio 16****/ 
-
-SELECT CLIENTES.COD_CLIENTE, CLIENTES.NOME, COUNT(LOCACOES.COD_LOCACAO) AS Total_Locacoes
-
-FROM CLIENTES
-
-JOIN LOCACOES  ON CLIENTES.COD_CLIENTE = LOCACOES.COD_CLIENTE
-
-GROUP BY CLIENTES.COD_CLIENTE, CLIENTES.NOME
-
-HAVING COUNT(LOCACOES.COD_LOCACAO) > 3;
+select CLIENTES.COD_CLIENTE, CLIENTES.NOME, count(LOCACOES.COD_LOCACAO) as Total_Locacoes
+from CLIENTES
+JOIN LOCACOES  on CLIENTES.COD_CLIENTE = LOCACOES.COD_CLIENTE
+group by CLIENTES.COD_CLIENTE, CLIENTES.NOME
+having count(LOCACOES.COD_LOCACAO) > 3;
 
 /****Exercicio 17****/ 
-
-SELECT c.NOME, SUM(f.VALOR_LOCACAO) AS Total_Gasto
-
-FROM CLIENTES c
-
+select c.NOME, sum(f.VALOR_LOCACAO) AS Total_Gasto
+from CLIENTES c
 JOIN LOCACOES l ON c.COD_CLIENTE = l.COD_CLIENTE
-
 JOIN FILME f ON l.COD_FILME = f.COD_FILME
-
-WHERE c.NOME = 'Renata Cristina'
-
-GROUP BY c.NOME;
+where c.NOME = 'Renata Cristina'
+group by c.NOME;
 
 /****Exercicio 18****/ 
-
-SELECT AVG(VALOR_LOCACAO) AS Media_Valor_Locacao
-
-FROM FILME;
+select avg(VALOR_LOCACAO) as Media_Valor_Locacao
+from FILME;
 
 /****Exercicio 19****/ 
-
-SELECT MIN(VALOR_LOCACAO) AS Menor_Valor_Locacao
-
-FROM FILME;
-
+select min(VALOR_LOCACAO) as Menor_Valor_Locacao
+from FILME;
 select * from FILME
 
 /****Exercicio 20****/ 
-
-SELECT c.NOME_CATEGORIA, COUNT(f.COD_FILME) AS Quantidade_Filmes
-
-FROM CATEGORIA c
-
-JOIN FILME f ON c.COD_CATEGORIA = f.COD_CATEGORIA
-
-GROUP BY c.NOME_CATEGORIA;
+select c.NOME_CATEGORIA, count(f.COD_FILME) as Quantidade_Filmes
+from CATEGORIA c
+JOIN FILME f on c.COD_CATEGORIA = f.COD_CATEGORIA
+group by c.NOME_CATEGORIA;
--- Banco de Dados/Exercicios04.sql
+++ Banco de Dados/Exercicios04.sql
@@ -1,16 +1,56 @@
 /****Exercicio 1****/
-SELECT COD_FILME, COUNT(DISTINCT DATA_RETIRADA) AS Quantidade_Aluguel
-FROM LOCACOES
-GROUP BY COD_FILME;
+select f.COD_FILME, count(l.DATA_RETIRADA) as Vezes_Retirada from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+group by f.COD_FILME
 
-SELECT F.FILME, L.DATA_DEVOLUCAO
-FROM LOCACOES L
-JOIN FILME F ON L.COD_FILME = F.COD_FILME
-JOIN CATEGORIA C ON F.COD_CATEGORIA = C.COD_CATEGORIA
-JOIN CLIENTES CL ON L.COD_CLIENTE = CL.COD_CLIENTE
-WHERE F.DIRETOR = 'Richard Donner'
-AND C.NOME_CATEGORIA = 'Ação'
-AND CL.NOME = 'Edson Martin Feitosa';
+/****Exercicio 2****/
+select f.FILME, l.DATA_DEVOLUCAO from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+inner join CLIENTES c on c.COD_CLIENTE = l.COD_CLIENTE
+where c.NOME = 'Edson Martin Feitosa' and f.DIRETOR = 'Richard Donner'
 
-Select F.filme from filme F where F.diretor = 'Richard Donner'
-SELECT L.DATA_DEVOLUCAO from locacoes l where l.COD_CLIENTE = 'Edson Martin Feitosa'
+/****Exercicio 3****/
+select FILME, (VALOR_LOCACAO * 0.10) as DESCONTO,
+(VALOR_LOCACAO - (VALOR_LOCACAO * 0.10)) as VALOR_FINAL
+from FILME
+
+/****Exercicio 4****/
+select distinct f.FILME from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+inner join CLIENTES c on c.COD_CLIENTE = l.COD_CLIENTE
+where c.CIDADE = 'Votorantim'
+
+/****Exercicio 5****/
+select f.FILME from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+where l.DATA_RETIRADA = GETDATE()
+
+/****Exercicio 6****/
+select f.FILME, (f.VALOR_LOCACAO * count(*)) as Lucro from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+where f.FILME = 'A vida é bela'
+group by f.FILME, f.VALOR_LOCACAO
+
+/****Exercicio 7****/
+select f.DIRETOR, count(l.DATA_RETIRADA) as Saidas from FILME f
+inner Join LOCACOES l on f.COD_FILME = l.COD_FILME
+group by f.DIRETOR
+
+/****Exercicio 8****/
+select f.FILME, cat.NOME_CATEGORIA from CATEGORIA cat
+inner join FILME f on f.COD_CATEGORIA = cat.COD_CATEGORIA
+inner join LOCACOES l on l.COD_FILME = f.COD_FILME
+inner join CLIENTES c on l.COD_CLIENTE = c.COD_CLIENTE
+where c.NOME = 'Maria Chiquinha'
+
+
+/****Exercicio 9****/
+select c.NOME, c.TELEFONE from CLIENTES c
+inner join LOCACOES l on l.COD_CLIENTE = c.COD_CLIENTE
+inner Join FILME f on f.COD_FILME = l.COD_FILME
+where f.FILME = 'Constantine'
+
+/****Exercicio 10****/
+select cat.NOME_CATEGORIA from CATEGORIA cat
+left join FILME f on f.COD_CATEGORIA = cat.COD_CATEGORIA
+where f.COD_CATEGORIA is null
