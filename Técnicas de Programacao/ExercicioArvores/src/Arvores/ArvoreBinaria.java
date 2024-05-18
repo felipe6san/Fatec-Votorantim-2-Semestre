99a946fe357ca2a11d2faea0233e233f55dd89ea
felipe6san <felipe6san@hotmail.com>
2024-05-17 21:22:04 -0300

    OrdemsED

--- Técnicas de Programacao/ExercicioArvores/src/Arvores/ArvoreBinaria.java
+++ Técnicas de Programacao/ExercicioArvores/src/Arvores/ArvoreBinaria.java
@@ -3,33 +3,96 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Arvores;
-
+ 
 /**
  *
- * @author Alunos
+ * @author feh
  */
 public class ArvoreBinaria {
+    //Atributos
     private NoDuploArvore raiz;
-    
-    public ArvoreBinaria(Object info){
+    public ArvoreBinaria(Object info) {
         raiz = new NoDuploArvore(info, null, null);
     }
-    
-    public void AddNoOrdenado(int valor, NoDuploArvore no){
-        if(valor < (int)no.getInfo()){
-            if(no.getEsquerdaNo() != null){
-                no.setEsquerdaNo(new NoDuploArvore(valor, null, null));
+    public int buscar(int valor) {
+        return buscar(valor, raiz, 0);
+    }
+    private int buscar(int valor, NoDuploArvore no, int profundidade) {
+        int resposta = -1;
+        if((int)no.getInfo() == valor) {
+            return profundidade;
+        }
+        else {
+            //verifica se o nó da esquerda ou da direita não é nulo
+            //se nao for nulo busca nesses nós
+            if(valor < (int)no.getInfo() && no.getEsquerdaNo() != null) {
+                resposta = buscar(valor, no.getEsquerdaNo(), profundidade++);
             }
-           else{
-            AddNoOrdenado(valor, no.getEsquerdaNo());
+            else if(no.getDireitaNo() != null) {
+                resposta = buscar(valor, no.getDireitaNo(), profundidade++);
+            }
+            return resposta;
         }
-        }
-        else{
-            if(no.getDireitaNo() != null) {
+    }
+    public void addNoOrdenado(int valor) {
+        addNoOrdenado(valor, raiz);
+    }
+    private void addNoOrdenado(int valor, NoDuploArvore no) {
+        //se valor for menor que o valor do nó adiciona a esquerda
+        //se nao adiciona a direita
+        if (valor < (int)raiz.getInfo()) {
+            if(no.getEsquerdaNo() == null) {
+                no.setEsquerdaNo(new NoDuploArvore(valor, null, null));
+            } else {
+                addNoOrdenado(valor, no.getEsquerdaNo());
+            }
+        } 
+        else {
+            if(no.getDireitaNo() == null) {
                 no.setDireitaNo(new NoDuploArvore(valor, null, null));
-        }   else{
-            AddNoOrdenado(valor, no.getDireitaNo());
+            } else {
+                addNoOrdenado(valor, no.getDireitaNo());
+            }
         }
     }
     
+    public void imprimirPreOrdem() {
+        imprimirPreOrdem(raiz);
+    }
+    
+    private void imprimirPreOrdem(NoDuploArvore no) {
+        System.out.println("No: " + no.getInfo());
+        if(no.getEsquerdaNo() != null)
+            imprimirPreOrdem(no.getEsquerdaNo());
+        if(no.getDireitaNo() != null)
+            imprimirPreOrdem(no.getDireitaNo());
+    }
+    
+    public void imprimirEmOrdem() {
+        imprimirEmOrdem(raiz);
+    }
+    
+    private void imprimirEmOrdem(NoDuploArvore no) {
+    
+        if(no.getEsquerdaNo() != null)
+            imprimirEmOrdem(no.getEsquerdaNo());
+        System.out.println("No: " + no.getInfo());
+        if(no.getDireitaNo() != null)
+            imprimirEmOrdem(no.getDireitaNo());
+    } 
+    
+    
+    public void imprimirPosOrdem() {
+        imprimirPosOrdem(raiz);
+    }
+    
+    private void imprimirPosOrdem(NoDuploArvore no) {
+        
+        if(no.getEsquerdaNo() != null)
+            imprimirPosOrdem(no.getEsquerdaNo());
+        
+        if(no.getDireitaNo() != null)
+            imprimirPosOrdem(no.getDireitaNo());
+        System.out.println("No: " + no.getInfo());
+    }
 }
